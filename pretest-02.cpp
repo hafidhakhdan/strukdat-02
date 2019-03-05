83c7ab360bb0f46a457e9f6b56fb79340b93295e
hafidhakhdan <hafidh606@gmail.com>
2019-03-05 08:46:16 +0700

    pretest

---
+++ pretest-02.cpp
@@ -0,0 +1,36 @@
+/*
+Nama	: Hafidh Akhdan Najib
+Kelas 	: A
+NPM		: 140810180061
+Tanggal	: Selasa, 5 Maret 2019
+*/
+
+#include <iostream>
+
+using namespace std;
+
+void moveZeroToEnd (int arr[], int n){
+	for (int i=0; i<n; i++){
+		if (arr[i]   )
+	}
+}
+
+void input (int (&arr)[100], int& n){
+	cout<<"Input panjang data :"; cin>>n;
+	
+	for (int i = 0; i < n ; i++){
+		cout<<"Input data ke-"<<i+1<<" = "; cin>>arr[i]
+	}
+}
+
+void output (int arr [], int n){
+	
+}
+
+int main (){
+	int arr [100];
+	int n;
+	input (arr,n);
+	moveZeroToEnd (arr,n);
+	output (arr,n);
+}
